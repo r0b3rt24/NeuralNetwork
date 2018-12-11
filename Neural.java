5a486c08cb86ef1951a4f51efac9cce0c6cf7563
Robert Cao <ch.robert.26@gmail.com>
2018-12-10 20:40:29 -0600

    finished

--- Neural.java
+++ Neural.java
@@ -1,8 +1,53 @@
-import sun.jvm.hotspot.runtime.VM;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 class Neural{
+
+    private static class Student {
+        Double midterm;
+        Double HW;
+        Double A;
+
+        Student(String hw, String mid, String a) {
+            midterm = Double.parseDouble(mid);
+            HW = Double.parseDouble(hw);
+            A = Double.parseDouble(a);
+        }
+    }
+
+    private static ArrayList<Student> fileReader(int FLAG) {
+        String trainFileName = "./hw2_midterm_A_train.txt";
+        String testFileName = "./hw2_midterm_A_test.txt";
+        String evalFileName = "./hw2_midterm_A_eval.txt";
+        String fileName;
+        if (FLAG == 100) {
+            fileName = trainFileName;
+        } else if (FLAG == 200) {
+            fileName = evalFileName;
+        } else {
+            fileName = testFileName;
+        }
+
+        ArrayList<Student> res = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            while (scanner.hasNextLine()) {
+                String[] s = scanner.nextLine().split(" ");
+                Student student = new Student(s[0], s[1], s[2]);
+                res.add(student);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+
     public static void main(String[] args){
         // detect the corner case
         if (args.length == 0) {
@@ -48,9 +93,211 @@ class Neural{
             Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);
             printer(FLAG300OP);
 
+        } else if (args.length == 13 && args[0].equals("400")) {
+            Double[] weights = readWeights(args);
+
+            // read x1 and x2
+            Double x1 = Double.parseDouble(args[10]);
+            Double x2 = Double.parseDouble(args[11]);
+            Double y = Double.parseDouble(args[12]);
+
+            Double[] FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+            Double[] FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+            Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);  // Va' Ua' Vb' Ub'
+
+            Double[] FLAG400OP = FLag400Function(FLAG100OP, FLAG200OP, FLAG300OP, x1, x2);
+            printer(FLAG400OP);
+        } else if (args.length == 14 && args[0].equals("500")) {
+            Double[] weights = readWeights(args);
+
+            // read x1 and x2
+            Double x1 = Double.parseDouble(args[10]);
+            Double x2 = Double.parseDouble(args[11]);
+            Double y = Double.parseDouble(args[12]);
+            Double n = Double.parseDouble(args[13]);
+
+            Double[] FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+            Double[] FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+            Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);  // Va' Ua' Vb' Ub'
+            Double[] FLAG400OP = FLag400Function(FLAG100OP, FLAG200OP, FLAG300OP, x1, x2);  // w1' w2' ... w9'
+            Double[] FLAG500OP = newWs(n, weights, FLAG400OP);  // w1' w2' ... w9'
+            weightsPrinter(weights);
+            formatPrinter(FLAG200OP[0]);
+            weights = FLAG500OP;
+
+            weightsPrinter(weights);
+            FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+            FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+            formatPrinter(FLAG200OP[0]);
+        } else if (args.length == 11 && args[0].equals("600")) {
+            Double[] weights = readWeights(args);
+
+            // read x1 and x2
+            Double n = Double.parseDouble(args[10]);
+            ArrayList<Student> students = fileReader(100);
+
+            for (Student s : students) {
+                Double x1 = s.HW;
+                Double x2 = s.midterm;
+                Double y = s.A;
+
+                Double[] sinfo = new Double[]{x1, x2, y};
+
+                Double[] FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+                Double[] FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+                Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);  // Va' Ua' Vb' Ub'
+                Double[] FLAG400OP = FLag400Function(FLAG100OP, FLAG200OP, FLAG300OP, x1, x2);  // w1' w2' ... w9'
+                Double[] FLAG500OP = newWs(n, weights, FLAG400OP);  // w1' w2' ... w9'
+                printer(sinfo);
+                System.out.println();
+                weights = FLAG500OP;
+                weightsPrinter(weights);
+                formatPrinter(evaluationSetE(weights));
+            }
+        } else if (args.length == 12 && args[0].equals("700")) {
+            Double[] weights = readWeights(args);
+
+            // read x1 and x2
+            Double n = Double.parseDouble(args[10]);
+            ArrayList<Student> students = fileReader(100);
+
+            int T = Integer.parseInt(args[11]);
+            for (int i = 0; i < T; i++) {
+                for (Student s : students) {
+                    Double x1 = s.HW;
+                    Double x2 = s.midterm;
+                    Double y = s.A;
+
+                    Double[] sinfo = new Double[]{x1, x2, y};
+
+                    Double[] FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+                    Double[] FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+                    Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);  // Va' Ua' Vb' Ub'
+                    Double[] FLAG400OP = FLag400Function(FLAG100OP, FLAG200OP, FLAG300OP, x1, x2);  // w1' w2' ... w9'
+                    Double[] FLAG500OP = newWs(n, weights, FLAG400OP);  // w1' w2' ... w9'
+                    weights = FLAG500OP;
+                }
+                weightsPrinter(weights);
+                formatPrinter(evaluationSetE(weights));
+            }
+        } else if (args.length == 12 && args[0].equals("800")) {
+            Double[] weights = readWeights(args);
+
+            // read x1 and x2
+            Double n = Double.parseDouble(args[10]);
+            ArrayList<Student> students = fileReader(100);
+            int count = 0;
+            Double prevE = Double.MAX_VALUE;
+            int T = Integer.parseInt(args[11]);
+            for (int i = 0; i < T; i++) {
+                count ++;
+                for (Student s : students) {
+                    Double x1 = s.HW;
+                    Double x2 = s.midterm;
+                    Double y = s.A;
+
+                    Double[] sinfo = new Double[]{x1, x2, y};
+
+                    Double[] FLAG100OP = Flag100Function(weights, x1, x2);  // ua, va, ub, vb, uc, vc
+                    Double[] FLAG200OP = Flag200Function(FLAG100OP[5], y);  // E, Vc', Uc'
+                    Double[] FLAG300OP = Flag300Function(weights, FLAG100OP, FLAG200OP);  // Va' Ua' Vb' Ub'
+                    Double[] FLAG400OP = FLag400Function(FLAG100OP, FLAG200OP, FLAG300OP, x1, x2);  // w1' w2' ... w9'
+                    Double[] FLAG500OP = newWs(n, weights, FLAG400OP);  // w1' w2' ... w9'
+                    weights = FLAG500OP;
+                }
+                if (evaluationSetE(weights) > prevE) {
+                    break;
+                }
+                prevE = evaluationSetE(weights);
+            }
+            System.out.println(count);
+            weightsPrinter(weights);
+            formatPrinter(evaluationSetE(weights));
+            formatPrinter(accuracy(weights));
+
+
         } else {
             System.out.println("Read the PDF for usage");
         }
+    }
+
+    static private Double accuracy (Double[] w) {
+        ArrayList<Student> test = fileReader(300);
+        int success = 0;
+        for (Student s : test) {
+            Double[] FLAG100OP = Flag100Function(w, s.HW, s.midterm);  // ua, va, ub, vb, uc, vc
+            int result;
+
+            if (FLAG100OP[5] >= 0.5) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+
+            if (result == s.A) {
+                success ++;
+            }
+        }
+        return 1.0*success/25;
+    }
+
+    static private Double evaluationSetE(Double[] w) {
+        ArrayList<Student> test = fileReader(200);
+        Double sum = 0.0;
+        for (Student s : test) {
+            Double[] FLAG100OP = Flag100Function(w, s.HW, s.midterm);  // ua, va, ub, vb, uc, vc
+            sum += 0.5 * Math.pow((FLAG100OP[5] - s.A), 2);
+        }
+
+        return sum;
+    }
+
+    static private void studentsPrinter(ArrayList<Student> students) {
+        for (Student s : students) {
+            System.out.println(s.midterm+" "+s.HW+" "+s.A);
+        }
+    }
+
+    static private void formatPrinter(Double w) {
+            String out =String.format("%.5f", w);
+            System.out.println(out);
+    }
+
+    static private void weightsPrinter(Double[] w) {
+        for (int i = 1; i < w.length; i ++) {
+            String out =String.format("%.5f", w[i]);
+            System.out.print(out+" ");
+        }
+        System.out.println();
+    }
+
+    static private Double[] newWs (Double n, Double[] oldw, Double[] pw) {
+        Double[] res = new Double[oldw.length];
+        for (int i = 1; i <oldw.length; i++) {
+            res[i] = oldw[i] - n * pw[i-1];
+        }
+        return res;
+    }
+
+    /**
+     * wij' = vi * uj'
+     * @param F100 ua, va, ub, vb, uc, vc
+     * @param F200 E, Vc', Uc'
+     * @param F300 Va' Ua' Vb' Ub'
+     * @return w1' w2' ... w9'
+     */
+    static private Double[] FLag400Function(Double[] F100, Double[] F200, Double[] F300, Double x1, Double x2) {
+        Double[] OP = new Double[9];  //w1' w2' ... w9'
+        OP[0] = 1.0 * F300[1];
+        OP[1] = x1 * F300[1];
+        OP[2] = x2 * F300[1];
+        OP[3] = 1.0 * F300[3];
+        OP[4] = x1 * F300[3];
+        OP[5] = x2 * F300[3];
+        OP[6] = 1.0 * F200[2];
+        OP[7] = F100[1] * F200[2];
+        OP[8] = F100[3] * F200[2];
+        return OP;
     }
 
     static private Double[] Flag300Function(Double[] w, Double[] FLAG100OP, Double[] FLAG200OP) {
@@ -63,10 +310,6 @@ class Neural{
     }
 
     static private Double partialHiddenVj(Double weights, Double partialUk) {
-//        Double sum = 0.0;
-//        for (Double d : weights) {
-//            sum += d * partialUk;
-//        }
         return weights * partialUk;
     }
 
